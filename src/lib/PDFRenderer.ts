7da6b03b4d9b6a8f844b5c80d67974f76a32f997
ProGM <progiemmeh@gmail.com>
2019-09-19 15:34:55 +0200

    Preparing for lambda deployment

--- src/lib/PDFRenderer.ts
+++ src/lib/PDFRenderer.ts
@@ -1,11 +1,18 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
 
 export default class PDFRenderer {
 	async renderURL(url: string, format: string = 'A4') {
-		const browser = await puppeteer.launch({ headless: true });
+		const executablePath = await chromium.executablePath;
+
+		const browser = await puppeteer.launch({
+			headless: true,
+			args: chromium.args,
+			executablePath,
+		});
 		const page = await browser.newPage();
 		await page.goto(url, { waitUntil: 'networkidle0' });
-		const pdf = await page.pdf({ format: format });
+		const pdf = await page.pdf({ format: format, printBackground: true });
 
 		await browser.close();
 		return pdf;
